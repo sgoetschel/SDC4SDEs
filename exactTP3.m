5a4dd939b722133c64f5f2465ca1be2a9d5a3be2
Sebastian Götschel <sebastian.goetschel@tuhh.de>
2026-09-01 15:18:17 +0200

    .

--- exactTP3.m
+++ exactTP3.m
@@ -2,13 +2,13 @@
 % this represents an approximation of the exact solution
 function [sol] = exactTP3(~, ~, t_begin, t_end, step_size, X0, eta)
 %Stratonovich-SDE
-    time = t_begin:step_size:t_end;
-    int = 0;
-    for i=1:length(time)-1
-        sol1(i) = X0*exp(0.5*time(i)+eta(1,i))./(1+X0*int);
-        int = int + (time(i+1)-time(i)).*exp(0.5.*time(i+1) + eta(1,i+1));
-    end
-    sol1(end+1) =  X0*exp(0.5*time(end)+eta(1,end))./(1+X0*int);
+    % time = t_begin:step_size:t_end;
+    % int = 0;
+    % for i=1:length(time)-1
+    %     sol1(i) = X0*exp(0.5*time(i)+eta(1,i))./(1+X0*int);
+    %     int = int + (time(i+1)-time(i)).*exp(0.5.*time(i+1) + eta(1,i+1));
+    % end
+    % sol1(end+1) =  X0*exp(0.5*time(end)+eta(1,end))./(1+X0*int);
 
     % fixed analytic sol approx
     nsteps = (t_end-t_begin)/step_size;
